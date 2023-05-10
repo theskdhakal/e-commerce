013befb2064f91ab92ac304fac6eea08d730b1d9
Shiva Dhakal <theskdhakal@gmail.com>
2023-05-10 13:40:01 +1000

    router dom installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Button } from "react-bootstrap";
 import "./App.css";
 import { ToastContaine, ToastContainer, toast } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
+import { Registration } from "./pages/registration/Registration";
+import { Login } from "./pages/login/Login";
+import { Dashboard } from "./pages/dashboard/Dashboard";
 
 function App() {
-  toast.success("gbhn");
   return (
-    <div className="App">
-      <Button variant="danger">"hello world"</Button>
+    <div className="">
+      <Routes>
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
       <ToastContainer />
     </div>
   );

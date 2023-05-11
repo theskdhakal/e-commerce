65fbaa32128b04c00d56c738351f0a918ecb3de6
Shiva Dhakal <theskdhakal@gmail.com>
2023-05-11 12:25:45 +1000

    registration part done

---
+++ src/assets/inputFields.js
@@ -0,0 +1,52 @@
+export const userRegisterInput = [
+  {
+    label: "First Name",
+    name: "fName",
+    type: "text",
+    placeholder: "Dwayne ",
+    required: true,
+  },
+  {
+    label: "Last Name",
+    name: "lName",
+    type: "text",
+    placeholder: " Johnson",
+    required: true,
+  },
+
+  {
+    label: "Address",
+    name: "address",
+    type: "text",
+    placeholder: "Rockdale,NSW",
+    required: true,
+  },
+  {
+    label: "Contact No.",
+    name: "contact",
+    type: "number",
+    placeholder: "04000000000",
+    required: true,
+  },
+  {
+    label: "Email",
+    name: "email",
+    type: "email",
+    placeholder: "Dwayne@Johnson.com",
+    required: true,
+  },
+  {
+    label: "Password",
+    name: "password",
+    type: "password",
+    placeholder: "XXXXXXXXXX",
+    required: true,
+  },
+  {
+    label: "Confirm Password",
+    name: "confirmPassword",
+    type: "password",
+    placeholder: "XXXXXXXXXX",
+    required: true,
+  },
+];
--- src/pages/registration/Registration.js
+++ src/pages/registration/Registration.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { CustomInput } from "../../components/custom-input/CustomInput";
+import { Button, Form } from "react-bootstrap";
+import { toast } from "react-toastify";
+import { userRegisterInput } from "../../assets/inputFields";
 
 export const Registration = () => {
-  return <div>Registration</div>;
+  const [form, setForm] = useState({});
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleOnSubmit = (e) => {
+    try {
+      e.preventDefault();
+
+      const { confirmPassword, password, ...rest } = form;
+      if (confirmPassword !== password) {
+        return toast.error("password do not match");
+      }
+
+      console.log(form);
+    } catch (error) {}
+  };
+
+  return (
+    <div>
+      <Form
+        onSubmit={handleOnSubmit}
+        className="mb-3 border p-5 shadow-lg rounded m-auto bg-light "
+        style={{ width: "400px" }}
+        controlId="formBasicEmail"
+      >
+        <h2 className="fw-bolder">Join the admin panel now!</h2>
+        {userRegisterInput.map((item, i) => (
+          <CustomInput key={i} {...item} onChange={handleOnChange} />
+        ))}
+
+        <div className="d-grid">
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };

ea484338ea38e66c2f472008df4e9b437e73f894
Shiva Dhakal <theskdhakal@gmail.com>
2023-05-12 15:36:18 +1000

    user authentication done using firebase auth service

--- src/pages/registration/registrationAction.js
+++ src/pages/registration/registrationAction.js
@@ -1,1 +1,23 @@
-// export const registrationAdmin
+export const registrationAdminAction = async (obj) => {
+  try {
+    //create auth user
+    const promiseUser = createUserWithEmailAndPassword(
+      auth,
+      obj.email,
+      obj.password
+    );
+
+    toast.promise(promiseUser, {
+      pending: "please wait....",
+      success: "User has been created successfully,please login now",
+    });
+
+    const { user } = await promiseUser;
+
+    if (user?.uid) {
+      //create user data and store in database
+    }
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
